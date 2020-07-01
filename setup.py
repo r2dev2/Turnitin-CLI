d6544a7a1468dd22764d5b34a1b802231d687de4
r2dev2bb8 <ronak.badhe@gmail.com>
2020-06-30 21:23:39 -0700

    Added pypi workflow

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import setuptools
 import sys
 
+# Thank you @KentoNishi for this setup.py
+
 try:
     with open("README.md", 'r') as fh:
         long_description = fh.read()
@@ -13,17 +15,17 @@ try:
 except:
     requirements = ''
 
-# versionName = sys.argv[1].replace("refs/tags/v", "")
-# del sys.argv[1]
+versionName = sys.argv[1].replace("refs/tags/v", "")
+del sys.argv[1]
 
 setuptools.setup(
     name="turnitin-cli",
-    version="1.3.0",
+    version=versionName,
     author="Ronak Badhe",
     author_email="ronak.badhe@gmail.com",
     description=long_description.split("\n")[1],
     long_description=long_description,
-    # long_description_content_type="text/markdown",
+    long_description_content_type="text/markdown",
     url="https://github.com/r2dev2bb8/Turnitin-CLI",
     packages=setuptools.find_packages(),
     classifiers=[
